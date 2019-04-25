a570a78640122aa79c018dfc10a0f4737a040426
Unknown <59010237@kmitl.ac.th>
2019-04-26 01:39:41 +0700

    new flex

--- parser.py
+++ parser.py
@@ -51,24 +51,36 @@ def p_expression_operators(p):
                   | term MOD term
                   | term MOD expression'''
     if p[2] == 'PLUS':
-        p[0] = p[1] + p[3]
+        p[0] = ('PLUS',p[1],p[3])
     elif p[2] == 'MINUS':
-        p[0] = p[1] - p[3]
+        p[0] = ('MINUS',p[1],p[3])
     elif p[2] == 'MUL':
-        p[0] = p[1] * p[3]
+        p[0] = ('MUL',p[1],p[3])
     elif p[2] == 'DIV':
-        p[0] = p[1] / p[3]
+        p[0] = ('DIV',p[1],p[3])
     elif p[2] == 'MOD':
-        p[0] = p[1] / p[3]
+        p[0] = ('MOD',p[1],p[3])
 
 def p_expression_negative(p):
     'expression : MINUS expression'
-    p[0] = ('minus', p[1])
+    p[0] = ('negative', p[1])
 
 
-def p_expression_bracket(p):
-    'expression : "LPAREN expression RPAREN'
+def p_expression_parenthesis(p):
+    'expression : LPAREN expression RPAREN'
     p[0] = p[2]
+
+def p_expression_assign(p):
+    '''exassign : TYPE_T EQU expression
+                  | TYPE_N EQU expression
+                  | expression EQU expression
+                  | '''
+    p[0] = ('assign',p[1],p[3])
+
+
+#----------------declare----------------------
+def p_term(p):
+    'term : TYPE_N EQU '
 
 
 # Error rule for syntax errors
