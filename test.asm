a5840430523f43bb3e94921cf79b38caddc98510
nepheart <nepheart_2@hotmail.com>
2019-05-03 23:27:21 +0700

    Revert "aasd"
    This reverts commit 1f48de13e6ffa081180235abd7d7272c1f1f4383.

--- test.asm
+++ test.asm
@@ -3,10 +3,11 @@ extern printf
 extern fflush
 global main
 section .data
-a dd 'hello',0
+a db 1
+_STR0 db 'n', 0
 section .text
 main:
-mov rcx, a
+mov rcx, _STR0
 call printf
 xor rcx, rcx
 call fflush
